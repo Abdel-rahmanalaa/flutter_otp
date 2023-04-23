ba58d979e89b79b10be28cf76a0af98e9e872c38
Abdel-rahmanalaa <Abdelrahmanalaa9911.com>
2023-04-23 10:16:08 +0200

    new screen for showVerification sucsess

---
+++ lib/presentation/screens/verification_screen.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_otp/app/shared_components.dart';
+import 'package:flutter_otp/controller/cubit/phone_Auth/cubit/phone_auth_cubit.dart';
+import 'package:flutter_otp/presentation/widgets/custom_elevated_button.dart';
+import 'package:flutter_otp/presentation/widgets/custom_text.dart';
+import 'package:flutter_otp/resources/app_colors.dart';
+import 'package:flutter_otp/resources/app_fonts.dart';
+import 'package:flutter_otp/resources/app_routes.dart';
+import 'package:flutter_otp/resources/app_strings.dart';
+import 'package:flutter_otp/resources/app_values.dart';
+
+// ignore: must_be_immutable
+class VerifyScreen extends StatelessWidget {
+  VerifyScreen({super.key});
+
+  PhoneAuthCubit phoneAuthCubit = PhoneAuthCubit();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          margin: const EdgeInsets.symmetric(
+              vertical: AppMargin.m100, horizontal: AppMargin.m20),
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: AppSize.s100,
+              ),
+              _buildVerifiedIcon(),
+              const SizedBox(
+                height: AppSize.s20,
+              ),
+              _buildVerificationText(),
+              const SizedBox(
+                height: AppSize.s250,
+              ),
+              _buildSignOutButton(context),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildVerifiedIcon() {
+    return const Icon(
+      Icons.verified,
+      color: AppColors.blue,
+      size: AppSize.s140,
+    );
+  }
+
+  Widget _buildVerificationText() {
+    return CustomText(
+      text: AppStrings.verificationText,
+      textColor: AppColors.black,
+      textSize: AppFontSize.s28,
+      textWieght: AppFontWight.bold,
+      textAlign: TextAlign.center,
+    );
+  }
+
+  Widget _buildSignOutButton(BuildContext context) {
+    return BlocProvider<PhoneAuthCubit>(
+      create: (context) => phoneAuthCubit,
+      child: CustomElevatedButton(
+        onPressed: () async {
+          await phoneAuthCubit.logOut();
+          if (context.mounted) {
+            navigatePushReplacment(context, RoutesName.loginRoute);
+          }
+        },
+        buttonWidth: double.infinity,
+        buttonChild: CustomText(
+          text: AppStrings.logOut,
+          textColor: AppColors.white,
+          textSize: AppFontSize.s16,
+        ),
+      ),
+    );
+  }
+}
